f4f4199fc409c014e6708d4e8b78ba084e822092
Kulasangar <kulasangar91@hotmail.com>
2020-08-21 23:42:07 +0530

    removed unnecessary resources in serverless template

--- aws-cognito/cognito_triggers/pre_token_generation.js
+++ aws-cognito/cognito_triggers/pre_token_generation.js
@@ -1,13 +1,13 @@
 exports.handler = (event, context, callback) => {
-    // event.response = {
-    //     "claimsOverrideDetails": {
-    //         "claimsToAddOrOverride": {
-    //             "attribute_key2": "attribute_value2",
-    //             "attribute_key": "attribute_value"
-    //         },
-    //         "claimsToSuppress": ["email"]
-    //     }
-    // };
+    event.response = {
+        "claimsOverrideDetails": {
+            "claimsToAddOrOverride": {
+                "attribute_key2": "attribute_value2",
+                "attribute_key": "attribute_value"
+            },
+            "claimsToSuppress": ["email"]
+        }
+    };
 
     // Return to Amazon Cognito
     callback(null, event);
