61c997591d66170898e7d0018c5c731c1aa562fb
Kulasangar <kulasangar91@hotmail.com>
2020-11-06 23:05:20 +0530

    added presigned url for get object

--- aws-media-upload--sls/presigned_url_generation.js
+++ aws-media-upload--sls/presigned_url_generation.js
@@ -12,21 +12,28 @@ exports.handler = (event, context, callback) => {
   var key = 'videos' + '/' + fileName;
   const signedUrlExpireSeconds = 60 * 5;
     
-  const url = s3.getSignedUrl('putObject', {
+  const put_url = s3.getSignedUrl('putObject', {
     Bucket: bucketName,
     Key: key,
     Expires: signedUrlExpireSeconds,
   });
 
-  console.log('presigned url: ', url);  
-  
+  const get_url = s3.getSignedUrl('getObject', {
+    Bucket: bucketName,
+    Key: key,
+    Expires: signedUrlExpireSeconds,
+  });
+
+  console.log('presigned put url: ', put_url);  
+  console.log('presigned get url: ', get_url);  
+
   const response = {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*', // Required for CORS support to work
       'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
     },
-    body: JSON.stringify({ filename: fileName,  presigned_url: url}),
+    body: JSON.stringify({ filename: fileName,  presigned_put_url: put_url,presigned_get_url: get_url}),
   };
 
   callback(null, response);
